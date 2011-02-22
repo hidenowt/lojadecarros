2ebf2a29eba255b8847df318c9ded0b34d9ff7f9
Herminio Torres <herminiocesar@gmail.com>
2011-02-22 20:08:01 -0300

    2.4 - Exercicios: Criando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lojadecarros::Application.config.secret_token = 'f6cebec5c4f814b121715a0e486fdd32e768f8fd9bd911357ae9b331406d354da4e65b016eee4a452dd63b4fc1d7381cf3e36415188112547566f9f14916b7de'
